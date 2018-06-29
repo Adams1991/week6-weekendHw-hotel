f51971a80207e37148ebe260ad5376c4232194bf
Adams1991 <adams121@hotmail.co.uk>
2018-06-29 14:08:41 +0100

    hotel test setup complete

--- src/main/java/Hotel/Hotel.java
+++ src/main/java/Hotel/Hotel.java
@@ -1,4 +1,24 @@
 package Hotel;
 
+import Hotel.RoomTypes.Bedroom;
+import Hotel.RoomTypes.ConferenceRoom;
+import Hotel.RoomTypes.DiningRoom;
+
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+    private ArrayList<DiningRoom> diningRooms;
+    private double takings;
+
+    public Hotel(ArrayList<Bedroom> bedrooms, ArrayList<ConferenceRoom> conferenceRooms,
+                 ArrayList<DiningRoom> diningRooms, double takings){
+                     this.bedrooms = bedrooms;
+                     this.conferenceRooms = conferenceRooms;
+                     this.diningRooms = diningRooms;
+                     this.takings = takings;
+    }
+
 }
---
+++ src/test/java/Hotel/HotelTest.java
@@ -0,0 +1,44 @@
+package Hotel;
+
+import Hotel.People.Guest;
+import Hotel.RoomTypes.Bedroom;
+import Hotel.RoomTypes.ConferenceRoom;
+import Hotel.RoomTypes.DiningRoom;
+import Hotel.RoomTypes.RoomTypes;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class HotelTest {
+
+
+    ArrayList<Bedroom> bedrooms;
+    ArrayList<DiningRoom> diningRooms;
+    ArrayList<ConferenceRoom> conferenceRooms;
+    Hotel hotel;
+    Guest guest;
+    ArrayList<Guest> guests;
+    Bedroom bedroom;
+    DiningRoom diningRoom;
+    ConferenceRoom conferenceRoom;
+
+
+
+    @Before
+    public void setUp() {
+        guest = new Guest("Bob", 400);
+        guests = new ArrayList<>();
+        bedrooms = new ArrayList<>();
+        diningRooms = new ArrayList<>();
+        conferenceRooms = new ArrayList<>();
+        bedroom = new Bedroom(guests, RoomTypes.SINGLE, 300);
+        conferenceRoom = new ConferenceRoom(50, guests, "Bezo Room", 150);
+        diningRoom = new DiningRoom(12, guests);
+        hotel = new Hotel(bedrooms,conferenceRooms,diningRooms, 100000);
+    }
+
+
+
+}
