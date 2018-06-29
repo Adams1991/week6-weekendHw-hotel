7db0cde4d39e5a2b5c3323016a1e8c76ceae3896
Adams1991 <adams121@hotmail.co.uk>
2018-06-29 11:01:27 +0100

    Dining room class basic setup done. TDD next

--- src/main/java/Hotel/RoomTypes/DiningRoom.java
+++ src/main/java/Hotel/RoomTypes/DiningRoom.java
@@ -1,8 +1,16 @@
 package Hotel.RoomTypes;
 
+import Hotel.People.Guest;
 import Hotel.Room;
 
+import java.util.ArrayList;
+
 public class DiningRoom extends Room {
+
+
+    public DiningRoom(int capacity, ArrayList<Guest> guests) {
+        super(capacity, guests);
+    }
 
 
 }
